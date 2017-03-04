fd0278bf0b0a701e7d2738dea0b4b4d59a5c4877
Jason Worden <wordenjasonp@gmail.com>
2017-03-04 12:10:53 -0800

    navbar branding

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -97,11 +97,6 @@ export default class App extends Component {
             </Nav>
             {user &&
             <p className={styles.loggedInMessage + ' navbar-text'}>Logged in as <strong>{user.email}</strong>.</p>}
-            <Nav navbar pullRight>
-              <NavItem eventKey={1} target="_blank" title="View on Github" href="https://github.com/erikras/react-redux-universal-hot-example">
-                <i className="fa fa-github"/>
-              </NavItem>
-            </Nav>
           </Navbar.Collapse>
         </Navbar>
 
@@ -109,12 +104,6 @@ export default class App extends Component {
           {this.props.children}
         </div>
 
-        <div className="well text-center">
-          Have questions? Ask for help <a
-          href="https://github.com/erikras/react-redux-universal-hot-example/issues"
-          target="_blank">on Github</a> or in the <a
-          href="https://discord.gg/0ZcbPKXt5bZZb1Ko" target="_blank">#react-redux-universal</a> Discord channel.
-        </div>
       </div>
     );
   }
