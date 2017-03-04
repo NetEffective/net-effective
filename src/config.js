dbd06664d31364deca8f9e256292f3c8d0c92835
Sarah Federman <sarahbethfederman@gmail.com>
2017-03-04 13:42:21 -0800

    deleted stuff/finagling with design

--- src/config.js
+++ src/config.js
@@ -15,7 +15,7 @@ module.exports = Object.assign({
   apiHost: process.env.APIHOST || 'localhost',
   apiPort: process.env.APIPORT,
   app: {
-    title: 'NetEffective',
+    title: 'neteffective',
     description: 'All the modern best practices in one example.',
     head: {
       titleTemplate: 'React Redux Example: %s',
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -64,7 +64,7 @@ export default class App extends Component {
         <Navbar fixedTop>
           <Navbar.Header>
             <Navbar.Brand>
-              <IndexLink to="/" activeStyle={{color: '#33e0ff'}}>
+              <IndexLink to="/">
                 <span>{config.app.title}</span>
               </IndexLink>
             </Navbar.Brand>
