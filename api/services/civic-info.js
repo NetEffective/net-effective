f9e8bbb531b4efa7c95e6fed8bb9bf384e8db629
Ben Buckman <ben@benbuckman.net>
2017-03-04 16:22:08 -0800

    Abstract out request with promise, and call civic info API.

--- api/services/civic-info.js
+++ api/services/civic-info.js
@@ -1,2 +1,36 @@
 'use strict';
 
+const config = require('../../src/config');
+
+const requestPromised = require('../utils/request-promised');
+
+
+function getStateRepresentatives(addressStr) {
+  const url = _buildRepsUrl(config.apiKeys.googleCivicInfoApi, addressStr);
+
+  return requestPromised({
+    method: 'GET',
+    url,
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .then((response) => {
+    console.log(response.body);
+
+    // TODO parse response.body ...
+    // return array of *state* representatives
+
+  });
+}
+
+
+function _buildRepsUrl(apiKey, addressStr) {
+  addressStr = encodeURIComponent(addressStr);
+  return `https://www.googleapis.com/civicinfo/v2/representatives?key=${apiKey}&address=${addressStr}`;
+}
+
+
+module.exports = {
+  getStateRepresentatives
+};
